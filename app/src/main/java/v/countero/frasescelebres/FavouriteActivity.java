fbfc45550f8d05743ce36a4d6b4a291671bdc3f3
Countero <javicg239@gmail.com>
2020-02-03 20:35:04 +0100

    2B ejercicios 5,7,8 hechos

--- app/src/main/java/v/countero/frasescelebres/FavouriteActivity.java
+++ app/src/main/java/v/countero/frasescelebres/FavouriteActivity.java
@@ -1,10 +1,14 @@
 package v.countero.frasescelebres;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class FavouriteActivity extends AppCompatActivity {
@@ -24,5 +28,37 @@ public class FavouriteActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_favourite, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_clear_all:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setIcon(android.R.drawable.stat_sys_warning);
+                builder.setTitle(R.string.menu_favourite_dialog_title);
+                builder.setMessage(R.string.menu_favourite_dialog_message);
+                builder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                break;
+        }
+        return true;
     }
 }
