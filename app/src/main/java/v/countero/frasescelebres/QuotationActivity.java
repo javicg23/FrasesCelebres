4bd4874dc834b7277041a8a808541172aa9f1657
Victor Cardona <vicardona7@hotmail.com>
2020-02-10 21:39:27 +0100

    pract3: settings con Preferences y nuevo diseno, y almacenar el estado actual, error con la visibilidad del menu add

--- app/src/main/java/v/countero/frasescelebres/QuotationActivity.java
+++ app/src/main/java/v/countero/frasescelebres/QuotationActivity.java
@@ -2,7 +2,10 @@ package v.countero.frasescelebres;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +15,18 @@ import android.widget.Toast;
 public class QuotationActivity extends AppCompatActivity {
 
     private TextView tvQuotation, tvAuthor;
+    private int nQuotationReceived = 0;
+    private MenuItem menuAdd;
+    private String quote, author;
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("quote", quote);
+        outState.putString("author", author);
+        outState.putInt("nQuotation", nQuotationReceived);
+        outState.putBoolean("addVisible", menuAdd.isVisible());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +36,35 @@ public class QuotationActivity extends AppCompatActivity {
         tvQuotation = findViewById(R.id.tvQuotation);
         tvAuthor = findViewById(R.id.tvAuthor);
 
-        String user = getString(R.string.username);
-        tvQuotation.setText(tvQuotation.getText().toString().replace("%1s", user));
+        if (savedInstanceState == null) {
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+            String user = prefs.getString("username", "");
+            if (user.equals("")) {
+                user = getString(R.string.username);
+            }
+            tvQuotation.setText(tvQuotation.getText().toString().replace("%1s", user));
+        } else {
+            tvQuotation.setText(savedInstanceState.getString("quote"));
+            tvAuthor.setText(savedInstanceState.getString("author"));
+            //menuAdd.setVisible(savedInstanceState.getBoolean("addVisible"));
+            nQuotationReceived = savedInstanceState.getInt("nQuotation");
+        }
     }
 
-    public void newQuotation(View view) {
-        tvAuthor.setText(getString(R.string.sample_autor));
-        tvQuotation.setText(getString(R.string.sample_quotation));
+    public void newQuotation(View view, int nQuotation) {
+        quote = getString(R.string.sample_quotation).replace("%1$d", nQuotation+"");
+        author = getString(R.string.sample_autor).replace("%1$d", nQuotation+"");
+        tvAuthor.setText(author);
+        tvQuotation.setText(quote);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_quotation, menu);
+        menuAdd = menu.findItem(R.id.menu_add);
+        menuAdd.setVisible(true);
         return true;
     }
 
@@ -42,9 +74,11 @@ public class QuotationActivity extends AppCompatActivity {
             case android.R.id.home:
                 return super.onOptionsItemSelected(item);
             case R.id.menu_add:
+                menuAdd.setVisible(false);
                 break;
             case R.id.menu_refresh:
-                newQuotation(item.getActionView());
+                nQuotationReceived++;
+                newQuotation(item.getActionView(), nQuotationReceived);
                 super.onOptionsItemSelected(item);
         }
         Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
--- app/src/main/java/v/countero/frasescelebres/SettingsActivity.java
+++ app/src/main/java/v/countero/frasescelebres/SettingsActivity.java
@@ -1,14 +1,35 @@
 package v.countero.frasescelebres;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private EditText etNameUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flSettings, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            // Load the preferences from an XML resource
+            setPreferencesFromResource(R.xml.preferences_settings, rootKey);
+        }
     }
 }
